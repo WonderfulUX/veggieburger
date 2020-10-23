80413f78ea390a7e621baff8417166446c7a22cd
Boris Boulogne <bboulogne@allmol.com>
2020-10-23 14:24:33 -0400

    restoV2 desktob only no cart process

---
+++ functions.js
@@ -0,0 +1,19 @@
+var actualLocation = document.getElementById('actual');
+
+function changeLoc(newLoc) {
+    let newLocValue = newLoc.innerText;
+    actualLocation.innerText=newLocValue;
+}
+
+function activeSubMenu(item) {
+    let y = document.querySelectorAll('#laCarte li');
+    y=y.length+1;
+    for (i=1; i<y; i++){
+        let x = "eleMenu"+i;
+        let ele = document.getElementById(x);
+        ele.style.color = "#373737";
+        ele.style.fontWeight = "normal";
+    }
+    item.style.color= " #d68538";
+    item.style.fontWeight= "bold";
+}
